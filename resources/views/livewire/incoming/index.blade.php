2560c717afabeefe27f1eb6989a1aca611d1eb4c
Kunga Jigmey <wddev@tibet.net>
2021-08-24 09:50:21 +0530

    blank show files for rinchen

--- resources/views/livewire/incoming/index.blade.php
+++ resources/views/livewire/incoming/index.blade.php
@@ -2,6 +2,10 @@
     <div class="w-full flex flex-row justify-between py-2">
         <div class="text-xl uppercase text-leading font-bold text-gray-700">Incoming file</div>
         <div>
+            <a class="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded py-2.5 hover:no-underline mr-3" 
+            href="{{ route('admin.show')}}">
+                Show
+            </a>
             <a class="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded py-2.5 hover:no-underline" 
             href="{{ url('admin/incomings/create')}}">
                 Create
