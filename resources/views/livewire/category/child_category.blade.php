bdc44389838ff27f6c92de7d74d5b339114baa70
Kunga Jigmey <wddev@tibet.net>
2021-08-24 10:45:11 +0530

    display for caregories in table

--- resources/views/livewire/category/child_category.blade.php
+++ resources/views/livewire/category/child_category.blade.php
@@ -1,7 +1,24 @@
-<li class="text-sm">{{ $child_category->title }}</li>
+<tr>
+    <td class="pl-4 py-2 text-sm border">{{$parent_iteration}}.{{$loop->iteration}}</td>
+    <td class="px-2 py-2 text-sm border">
+        {{$child_category->title}}
+        <p class="text-xs text-gray-500">{{$child_category->parent->title}}</p>
+    </td>
+    <td class="px-2 py-2 text-sm border text-center">
+        <div class="w-full flex flex-row justify-around">
+            <a href="{{url('admin/categories')}}/{{$child_category->id}}/edit">
+                <i class="fas fa-edit"></i>
+            </a>
+            <button type="button" wire:click="$set('delete_id', {{ $child_category->id }})" wire:loading.attr="disabled" data-toggle="modal" data-target="#delete_modal">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </div>
+    </td>
+</tr>
+@php
+    $parent = $parent_iteration.'.'.$loop->iteration;
+@endphp
+@foreach ($child_category->child as $childCategory)
+    @include('livewire.category.child_category', ['child_category' => $childCategory,'parent_iteration'=>$parent])
+@endforeach
 
-<ol class="pl-6 ml-auto list-disc list-inside bodyig">
-    @foreach ($child_category->child as $childCategory)
-        @include('livewire.category.child_category', ['child_category' => $childCategory])
-    @endforeach
-</ol>
--- resources/views/livewire/category/index.blade.php
+++ resources/views/livewire/category/index.blade.php
@@ -1,10 +1,38 @@
-<ol class="w-full ml-auto list-decimal list-inside bodyig">
-    @foreach ($categories as $category)
-        <li class="text-lg">{{ $category->title }}</li>
-        <ol class="pl-6 ml-auto list-disc list-inside bodyig">
-        @foreach ($category->child as $childCategory)
-            @include('livewire.category.child_category', ['child_category' => $childCategory])
-        @endforeach
-        </ol>
-    @endforeach
-</ol>
+<div>
+    <table class="w-full text-sm bg-white mt-3">
+        <thead>
+            <tr class="uppercase font-semibold text-left text-gray-900 bg-gray-200 border-t border-b border-gray-600">
+                <td class="pl-4 py-3 text-xs border-r">#</td>
+                <td class="px-2 py-3 text-xs border-r">Dispatched No</td>
+                <td class="px-2 py-3 text-xs border-r"></td>
+            </tr>
+        </thead>
+        <tbody class="bodyig">
+            @foreach($categories as $category)
+                <tr>
+                    <td class="pl-4 py-2 text-sm border font-semibold">{{$loop->iteration}}</td>
+                    <td class="px-2 py-2 text-sm border font-semibold">{{$category->title}}</td>
+                    <td class="px-2 py-2 text-sm border text-center">
+                        <div class="w-full flex flex-row justify-around">
+                            <a href="{{url('admin/categories')}}/{{$category->id}}/edit">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <button type="button" wire:click="$set('delete_id', {{ $category->id }})" wire:loading.attr="disabled" data-toggle="modal" data-target="#delete_modal">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+                @php 
+                    $parent = $loop->iteration;
+                @endphp
+                @foreach ($category->child as $childCategory)
+                    @include('livewire.category.child_category', 
+                    ['child_category' => $childCategory,'parent_iteration'=>$parent])
+                @endforeach
+            @endforeach 
+        </tbody>
+    </table>
+    @include('livewire.delete-modal')
+</div>
+ 
